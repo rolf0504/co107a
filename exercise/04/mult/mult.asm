52d870991549ee357884af8307351ad8607a5b40
rolf0504 <fangrolf@gmail.com>
2018-12-02 23:52:06 +0800

    1

--- exercise/04/mult/mult.asm
+++ exercise/04/mult/mult.asm
@@ -6,18 +6,18 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[3], respectively.)
 
+@R2
+M = 0
 (LOOP)
 @R1
 D = M
 @END
 D;JEQ
 
-@R1
-D = M
 @R0
-D = D + M
+D = M
 @R2
-M = D
+M = M + D
 
 @R1
 M = M - 1
